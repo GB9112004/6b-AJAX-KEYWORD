72a83fdb1f587dca9235dd01466b52a58cd5a6b9
G.BALAJI <145926244+GB9112004@users.noreply.github.com>
2024-10-02 19:41:41 +0530

    Add files via upload

---
+++ servertime.php
@@ -0,0 +1,7 @@
+<?php
+    // Set the content type to plain text
+    header("Content-Type: text/plain");
+
+    // Get the current server time
+    echo "Current Server Time: " . date("Y-m-d H:i:s");
+?>
